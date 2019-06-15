373e21c0af23510389fdbd58e32419be1d41cd7f
yamcha <dbals9812@naver.com>
2019-06-16 02:46:10 +0900

    Edit

--- 190524/allData.php
+++ 190524/allData.php
@@ -2,24 +2,20 @@
   $infoArray;
 
   $myfile = fopen("data.txt", "r");
-
   while(!feof($ofile)) {
     $aValue = fgets($ofile);
     $bValue = fgets($ofile);
     $infoArray[$aValue] = $bValue;
   }
-
   fclose($ofile);
 
   echo "<ul>";
-  foreach($infoArray as $x => $x_value) {
+  foreach($infoArray as $x => $x_value) {   //$infoArray에 값이 없을때까지 진행
     if ($x !== 0){
-          echo "<li>$x : ";
-          $re_value = rtrim($x_value, "\n");
-          echo "$re_value</li>";
+      echo "<li>$x : ";
+      $re_value = rtrim($x_value, "\n");
+      echo "$re_value</li>";
     }
-
   }
-   echo "</ul>";
-
+  echo "</ul>";
 ?>
--- 190524/dataFind.php
+++ 190524/dataFind.php
@@ -1,99 +1,62 @@
 <?php
+  $infoArray;   //값들을 저장할 배열
 
-$infoArray;
+  //"data.txt"를 읽어 배열에 값 넣기
+  $ofile = fopen("data.txt", "r");
+  while(!feof($ofile)) {
+    $aValue = fgets($ofile);
+    $bValue = fgets($ofile);
+    $infoArray[$aValue] = $bValue;
+  }
+  fclose($ofile);
 
-//"data.txt"를 읽어 배열에 값 넣기
+  //post방식으로 보낸 값을 받아오기
+  $fstr = $_POST['a1'];
+  $cstr = $_POST['b1'];
 
-$ofile = fopen("data.txt", "r");
-
-while(!feof($ofile)) {
-  $aValue = fgets($ofile);
-  $bValue = fgets($ofile);
-  $infoArray[$aValue] = $bValue;
-}
-//var_dump($infoArray);
-
-fclose($ofile);
-
-// 웹 페이지에서 값 읽어 오기
-
- $fstr = $_POST['a1'];
- $cstr = $_POST['b1'];
- //trim($fstr);
- //trim($cstr);
- //$fullstr = "./data/".$fstr;
-
-//file 이름과 내용의 값을 가진경우
- if(!empty($fstr) && !empty($cstr)) {
-     $co = 0;
-     echo "<ul>";
-/*
-   for($x=0; $x < count($infoArray); $x++) {
-
-   }
- */
-
-  foreach($infoArray as $x => $x_value) {
-
-    if(strpos($x,$fstr) !==false && strpos($x_value, $cstr) !== false) {
+  echo "<ul>";
+  if(!empty($fstr) && !empty($cstr)) {    //두 값이 모두 들어왔을 때
+    $co = 0;
+    foreach($infoArray as $x => $x_value) {
+      if(strpos($x,$fstr) !==false && strpos($x_value, $cstr) !== false) {
+        $co = $co +1;
+        echo "<li>$x : ";
+        $re_value = rtrim($x_value, "\n");
+        echo "$re_value</li>";
+      }
+    }
+    if ($co == 0) {
+      echo "No file";
+    }
+  } else if (!empty($fstr) && empty($cstr)) {   //파일 이름만 들어왔을 때
+    $co = 0;
+    foreach($infoArray as $x => $x_value) {
+      if(strpos($x,$fstr) !==false) {
       $co = $co +1;
       echo "<li>$x : ";
       $re_value = rtrim($x_value, "\n");
       echo "$re_value</li>";
+      }
     }
+    if ($co == 0) {
+      echo "No file";
+    }
+  } else if(empty($fstr) && !empty($cstr)) {    //내용만 들어왔을 때
+    $co = 0;
+    foreach($infoArray as $x => $x_value) {
+      if(strpos($x_value,$cstr) !==false) {
+        $co = $co +1;
+        echo "<li>$x : ";
+        $re_value = rtrim($x_value, "\n");
+      echo "$re_value</li>";
+      }
+    }
+    if ($co == 0) {
+      echo "No file";
+    }
+  } else {    //아무 값도 들어오지 않았을 때
+    echo "Enter the keywords of list that you want to search.";
   }
-
-  if ($co == 0) {
-    echo "No file";
-  }
-
-}
-
-// 화일 이름만 있을 경우
-else if (!empty($fstr) && empty($cstr)) {
-
- $co = 0;
- echo "<ul>";
-
- foreach($infoArray as $x => $x_value) {
-
-   if(strpos($x,$fstr) !==false) {
-     $co = $co +1;
-     echo "<li>$x : ";
-     $re_value = rtrim($x_value, "\n");
-     echo "$re_value</li>";
-   }
- }
-
- if ($co == 0) {
-   echo "No file";
- }
-
-}
-
-else if(empty($fstr) && !empty($cstr)) {
-
- $co = 0;
- echo "<ul>";
-
- foreach($infoArray as $x => $x_value) {
-
-   if(strpos($x_value,$cstr) !==false) {
-     $co = $co +1;
-     echo "<li>$x : ";
-     $re_value = rtrim($x_value, "\n");
-     echo "$re_value</li>";
-   }
- }
-
- if ($co == 0) {
-   echo "No file";
- }
-
-}
-else {
- echo "Enter the keywords of list that you want to search.";
-}
- echo "</ul>";
+  echo "</ul>";
 
 ?>
